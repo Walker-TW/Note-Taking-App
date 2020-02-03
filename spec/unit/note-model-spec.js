c668f0539f0761cc705b708b7fa574327cac9931
Walker-TW <thomas.w.walker@live.co.uk>
2020-02-03 18:24:34 +0000

    Refactored error messages

--- spec/unit/note-model-spec.js
+++ spec/unit/note-model-spec.js
@@ -3,30 +3,24 @@
 (function specNote(exports){
   function testIfNoteExist() {
     var notebook = new Notebook("");
-    if (notebook.note !== "") {
-      throw new Error("Note exists not!")
-    }
+      assert.isTrue(notebook.note == "")
+
     var a = notebook.note
     var b = []    
-    if(a != b){throw new Error("Romeo Romeo You're dead")} 
+    assert.isTrue(a == b)
   }
-  testIfNoteExist(console.log('test if note exist passed'));
-  // console.log('test if note exist passed');
+  testIfNoteExist()
+
   function callsNoteString() { 
     var notebook = new Notebook("My favourite language is Javacript"); 
-    if (notebook.returnNote() !== "My favourite language is Javacript") {
-      throw new Error("Could not retrieve")
-    }
-    console.log("callsNoteString passed");
+    assert.isTrue(notebook.returnNote() === "My favourite language is Javacript") 
   }
   callsNoteString()
 
   function testIfSave(){
     let notebook = new Notebook("My favourite language is Javacript")
     notebook.save()
-    if(notebook.returnNote() != ["My favourite language is Javacript"]){
-      throw new Error("You suck")
-    }
+    assert.isTrue(notebook.returnNote() == ["My favourite language is Javacript"])
   }
   testIfSave()
 
@@ -34,10 +28,9 @@
     let notebook = new Notebook("")
     notebook.save()
     let b = notebook.storage
-    if(notebook.returnStorage() != b ){
-      throw new Error("returnStorage does not work")
+    assert.isTrue(notebook.returnStorage() == b )
     }
-  }
+  
   testReturnStorage()
 
   function testInsertNote(){
@@ -45,9 +38,7 @@
     notebook.insertNote("My favourite language is Javacript")
     let a = notebook.storage[notebook.storage.length-1]
     let b = "My favourite language is Javacript"
-    if(a != b){
-      throw new Error("ERRRORRRRRRAAAAAAAAAAA")
-    }
+    assert.isTrue(a == b)
   }
   testInsertNote()
 
